2f1b40dfbce03ebdb93735f1480980525a619735
taufique <taufiquehr@gmail.com>
2020-03-18 06:44:34 +0600

    copy features

--- search/views.py
+++ search/views.py
@@ -5,7 +5,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.renderers import TemplateHTMLRenderer
-
+from pyzipcode  import ZipCodeDatabase
 from .models import Count,SP,Calculator,Appliance,System,WholeHome,SurgeProtect,LineProtect,ElectronicsProtect,Optional,Discount
 import json
 import requests
@@ -204,7 +204,7 @@ class DirectoryTable(APIView):
 
 
 
-from pyzipcode  import ZipCodeDatabase
+
 class CalculatorView(APIView):
     renderer_classes = [TemplateHTMLRenderer]
     template_name = 'calculator.html'
